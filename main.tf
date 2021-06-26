321a53ef306f92e3664a8b85bb2cab61840bee77
David Constenla <david.constenla@smava.de>
2021-06-26 17:07:45 +0200

    Add some terraform

---
+++ main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_version = "0.13.5"
+  backend "s3" {
+    role_arn     = "arn:aws:iam::1123455:role/atlantis"
+    session_name = "main-atlantis"
+    bucket       = "main-production-configuration"
+    key          = "terraform/state.tfstate"
+    encrypt      = "true"
+    kms_key_id   = "arn:aws:kms:eu-central-1:1123455:key/32598598585"
+    region       = "eu-central-1"
+  }
+}
